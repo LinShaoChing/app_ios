f5084776ce071e6770d61f863137f2e680c11435
LinShaoChing <mormor19971218@gmail.com>
2023-01-23 16:18:50 +0800

    Initial Commit

---
+++ app_ios/app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_iosApp.swift
+//  app_ios
+//
+//  Created by LinShaoChing on 2023/1/23.
+//
+
+import SwiftUI
+
+@main
+struct app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
